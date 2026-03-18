739d6e2757418f7f06229b9b718ae8e750e9e6f4
theworstengineer <kz.tippet@yahoo.com>
2026-03-18 17:06:02 +0000

    Add files via upload

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -9,18 +9,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if get_slide_collision(0) != null:
+		end_game()		
 	look_at(get_global_mouse_position())	
 	if rotation_degrees < limit_left: 
 		rotation_degrees = limit_left
 	if rotation_degrees > limit_right: 
 		rotation_degrees = limit_right
-	velocity = transform.x * speed
+	velocity = transform.x * speed * delta * 60
 	pass
-@export var speed = 400
-@export var angle_max = 60
-@export var angle_UP = -90
-@export var limit_right = angle_UP + angle_max
-@export var limit_left = angle_UP - angle_max
+const speed = 400
+const angle_max = 60
+const angle_UP = -90
+const limit_right = angle_UP + angle_max
+const limit_left = angle_UP - angle_max
 
 func _physics_process(delta):
 	move_and_slide()
+
+func end_game(): #empty for now but the collision detect works
+	1==1
